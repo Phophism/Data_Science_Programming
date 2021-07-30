23c2f09df48317b8dc6d2d157344c169e24657cf
akkapop prasompon <akkapop_mar@hotmail.com>
2021-07-31 02:25:48 +0700

    B1_v1
    Fix bug
    - Add greater case to  sorting function
    Add function
    - add_item()
    - list_to_int()
    Display result
    Add header detail

--- sorting/DS_Pro_HW_B1.py
+++ sorting/DS_Pro_HW_B1.py
@@ -1,33 +1,41 @@
+## Data Science Programming
+## Homework: B1
+## Akkapop Prasompon
+## 640631128
+## Create: July 30, 2021 
+## Last update: July 31, 2021 
+
+### Creat a function ###
+
 # Add number to the list
 # Also checking for negative interger
-
-# Create box that contain list of four positive integer
-box = list([-1])*4
-
-# for item = 0 to 3
-for item in range(0, len(box)):
-    input_text = "%s - Input positive integer : " % (item+1)
-    # if input is negative integer -> loop
-    while box[item] < 0:
-        box[item] = int(input(input_text))
-        if box[item] < 0:
-            print("Try again. Please input positive integer.")
-
-print(box)
+def add_item(list):
+    # for item = 0 to 3
+    for item in range(0, len(list)):
+        input_text = "%s - Input positive integer : " % (item+1)
+        # if input is negative integer -> loop
+        while list[item] < 0:
+            list[item] = int(input(input_text))
+            if list[item] < 0:
+                print("Try again. Please input positive integer.")
+    print('Your integers are : %s'% list )            
+    return list
 
 # swap function
 # list: list of number (box)
 # i: position n that want to swap
-def swap(list,i) :
+def bubble_swap(list,i) :
     temp = list[i+1]    
-    list[i+1] = list[i] # then swap j
-    list[i] = temp      # with j+1 
+    list[i+1] = list[i] 
+    list[i] = temp
     return list
 
+# I added "extended" because this function not just checking an integer number but also check a digit
+# Inspired by "extended kalman filter"
 def extended_bubble_sort(list):
-    for i in range(0, len(box), 1):
-        for j in range(0, len(box)-i-1, 1):
-            # Breaking element j and element j+1 into single digit
+    for i in range(0, len(list)-1, 1):
+        for j in range(0, len(list)-i-1, 1): # general bubble sort  algorithm
+            # Let's breaking an element j and j+1 into multiple digit
             # for example [10],[112] to [1,0],[1,1,2]
             a = str(list[j]) # turn integer in j to string for count a digit -> let call it "a"
             b = str(list[j+1]) # turn integer in j+1 to string for count a digit -> let call it "b"
@@ -38,21 +46,21 @@ def extended_bubble_sort(list):
             for k in range(0, len(b),1):
                 elm_b.append(b[k]) # breaking each digit of j+1 into list
             
-            # Sometimes j and j+1 having difference length
-            # To prevent an error "index out of range"
-            # I keep duplicating the last element of the shorter one until the number of length is equal to another element
-            if len(a) > len(b) :
-                diff = len(a)-len(b)
-                last_b = elm_b[len(b)-1]
+            # Sometimes j and j+1 have the difference length
+            # We want to compare it like a[0]<->b[0] , a[1]<->b[1] , ... , a[n]<->b[n]
+            # To prevent an error "index out of range", I keep duplicating the last element of the shorter one until the number of length is equal to another one
+            if len(a) > len(b) : 
+                diff = len(a)-len(b) # find a differnce of lenge of a and b (Ex. a=[1,2,3,4], b=[1,2,3] -> dif is 4-3 = 1)
+                last_b = elm_b[len(b)-1] # get the numbere of last element  (Ex. [1,2,3] -> last is 3)
                 for k in range(0,diff,1):
                     elm_b.append(last_b) # add an element until length is equal to "a"
                 # Condition part (Sorting)     
                 for k in range(0,len(a),1): # comparing a[0]<->b[0] , a[1]<->b[1] , ... , a[n]<->b[n]
-                    if elm_a[k] < elm_b[k] : # if a[x] < b[x]  
-                        temp = list[j+1]    
-                        list[j+1] = list[j] # then swap j
-                        list[j] = temp      # with j+1
+                    if elm_a[k] < elm_b[k] : # if a[x] < b[x] 
+                        list = bubble_swap(list,j) # then swap j with j+1 
                         break   # and exit loop to next j
+                    elif elm_a[k] > elm_b[k] : # else if a[x] > b[x] 
+                        break   # then exit loop and skip to next element
             elif len(a) < len(b) :
                 diff = len(b)-len(a)
                 last_a = elm_a[len(a)-1]
@@ -61,37 +69,40 @@ def extended_bubble_sort(list):
                 # Condition part (Sorting) 
                 for k in range(0,len(b),1):
                     if elm_a[k] < elm_b[k] :
-                        temp = list[j+1]
-                        list[j+1] = list[j]
-                        list[j] = temp
+                        list = bubble_swap(list,j)
                         break
-            elif len(a) == len(b) : # this case number of digits of "a" and "b" are equal. Hence, I don't need to add an element
+                    elif elm_a[k] > elm_b[k]:
+                        break
+            elif len(a) == len(b) : # in this case, the number of digits of "a" and "b" are equal. Hence, I don't need to add an element
                 # Condition part (Sorting) 
                 for k in range(0,len(a),1):
                     if elm_a[k] < elm_b[k] :
-                        temp = list[j+1]
-                        list[j+1] = list[j]
-                        list[j] = temp
+                        list = bubble_swap(list,j)
+                        break
+                    elif elm_a[k] > elm_b[k]:
                         break
     return list     # return result
-        
-result = extended_bubble_sort(box)       
-print(result)        
+
+def list_to_int(list):
+    text = ""
+    for i in range(0,len(list),1):
+        text += str(list[i])
+    return text        
+
+### End of Function creation ###
 
 
-# Bubble Sort
-    # In sorting condition
-    # If having same digit Then check()
-    # Else
-        # Extract into multiple one-digit integers (Ex. [112] -> [1,1,2])
-        # check() from left to right
-         # Ex. [10],[112]
-         # list_a[1,0] , list_b[1,1,2]
-         # list_a[0] <-> list_b[0]  ===  1 <-> 1 ===> equal
-         # if still equal
-         # list_a[1] <-> list_b[1] === 0 <-> 1 ===> more than
-         # then swap()
-         # if list_a[0] = list_b[0] and list_a[1] <-> list_b[1]
-         # list_a doesn't has list_a[2] then use list_a[1] <-> list_b[2]
+# Create box that contain list of four positive integer
+box = list([-1])*4
 
-# display result
+# Add items into the box
+add_item(box)
+
+# Calculate the highest integer        
+result = extended_bubble_sort(box)
+final_result = list_to_int(box)
+
+# Display the result       
+print('Com re-arranged to : ',result)        
+print('Final integer : ', final_result)
+
