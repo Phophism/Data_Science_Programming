9486ac996fb79662171b4cafe12db1889603f775
akkapop prasompon <akkapop_mar@hotmail.com>
2021-07-30 21:04:56 +0700

    Create DS_Pro_HW_B1.py
    Create home work B1 with a psuedocode

---
+++ sorting/DS_Pro_HW_B1.py
@@ -0,0 +1,23 @@
+# Add number to the list 
+# Also checking for negative interger
+
+
+# Bubble Sort
+    # In sorting condition
+    # If having same digit Then check()
+    # Else 
+        # Extract into multiple one-digit integers (Ex. [112] -> [1,1,2])
+        # check() from left to right
+            # Ex. [10],[112] 
+                # list_a[1,0] , list_b[1,1,2]
+                # list_a[0] <-> list_b[0]  ===  1 <-> 1 ===> equal
+                # if still equal
+                    # list_a[1] <-> list_b[1] === 0 <-> 1 ===> more than 
+                    # then swap()
+                # if list_a[0] = list_b[0] and list_a[1] <-> list_b[1]
+                # list_a doesn't has list_a[2] then use list_a[1] <-> list_b[2]
+                
+# display result        
+                
+# Talk is cheap, show me the code.
+                     
